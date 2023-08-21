98b49f26f12076917ac01fa206a15ee37bf80ad6
AliceOliveiraC <126628935+AliceOliveiraC@users.noreply.github.com>
2023-08-20 21:04:41 -0300

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,20 @@
+var firstName = "Rebeca";
+var lastName = "Kapoor";
+
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+console.log(add);
+
+var sub = a - b;
+console.log(sub);
+
+var multiple = a * b;
+console.log(multiple);
+
+var divide = a / b;
+console.log(divide);
